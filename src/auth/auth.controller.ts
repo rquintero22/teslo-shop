e08699ca5db23ebad52fc7c61a428f069a7be65b
Raymond Quintero <quinteroaparicio@gmail.com>
2022-08-22 16:55:00 -0600

    Se crea decoradores personalizados para los roles

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Res } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Res, SetMetadata } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { GetUser } from './decorators/get-user.decorator';
+import { RawHeaders } from './decorators/raw-headers.decorator';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('register')
   createUser(@Body() createUserDto: CreateUserDto) {
@@ -23,15 +25,29 @@ export class AuthController {
   @Get('private')
   @UseGuards(AuthGuard())
   testingPrivateRoute(
-    // @Res() request: Express.Request
-      @GetUser() user: User
-    ) {
-      // console.log(request.user);
+    // @Res() request: Express.Request,
+    @GetUser('email') user: User,
+    @RawHeaders() rawHeaders: string[]
+  ) {
+    // console.log(request.user);
     return {
       ok: true,
       message: 'Hola',
+      user,
+      rawHeaders
+    };
+  }
+
+  // @SetMetadata('roles', ['admin', 'super-user'])
+  @Get('private2')
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(
+    @GetUser() user: User
+  ) {
+    return {
+      ok: true,
       user
     };
   }
-  
+
 }
