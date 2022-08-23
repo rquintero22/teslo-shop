9c37417250b870d81927cc78086b8e2786ef3d2a
Raymond Quintero <quinteroaparicio@gmail.com>
2022-08-23 00:01:24 -0600

    Se implementa la documentacion del restapi con swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +13,15 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RestFull Api')
+    .setDescription('Teslo shop endpoint')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${process.env.PORT}`);
 }
